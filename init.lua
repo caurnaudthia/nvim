3b47b7f7d35079c54679d8c2544cbf750cd9f662
wisteria <derek.zhang@wisc.edu>
2024-03-06 14:38:08 -0600

    made changes to setup

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- functions file
-local ok1, funcs = pcall(require, 'functions.luarc')
-if not ok1 then print('luarc functions failed to load') end
+local _, funcs = pcall(require, 'functions.luarc')
+if not _ then print('luarc functions failed to load') end
 --funcs.protectedCall('plugins')
 
 -- options
@@ -71,131 +71,145 @@ lazy.setup({
       vim.cmd([[colorscheme rose-pine]])
     end,
   },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }, -- indentation hints
-  { 'nvim-tree/nvim-web-devicons' }, -- icons
-  { 'lewis6991/gitsigns.nvim' }, -- git actions
-  { "feline-nvim/feline.nvim" }, -- bottomline
-  { 'nanozuki/tabby.nvim' }, -- tab bar
+  {'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} }, -- indentation hints
+  {'nvim-tree/nvim-web-devicons', lazy = false}, -- icons
+  {'lewis6991/gitsigns.nvim', lazy = false}, -- git actions
+  {'feline-nvim/feline.nvim', lazy = false}, -- bottomline
+  {'nanozuki/tabby.nvim', lazy = false}, -- tab bar
 
   -- LANGUAGE SERVERS
-  {"williamboman/mason.nvim", lazy = false},
-  {"williamboman/mason-lspconfig.nvim", lazy = false},
-  {"neovim/nvim-lspconfig", lazy = false},
+  {'williamboman/mason.nvim', lazy = false},
+  {'williamboman/mason-lspconfig.nvim', lazy = false},
+  {'neovim/nvim-lspconfig', lazy = false},
 
   -- COMPLETION
   {'ms-jpq/coq_nvim', lazy = false, branch = 'coq'},
   {'ms-jpq/coq.artifacts', lazy = false, branch = 'artifacts'},
-  {'ms-jpq/coq.thirdparty', lazy = false, branch = '3p'}
+  {'ms-jpq/coq.thirdparty', lazy = false, branch = '3p'},
+
+  -- NAVIGATION
+  {'tpope/vim-repeat', lazy = false},
+  {'ggandor/leap.nvim', lazy = false},
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {}
+  },
+  {'stevearc/oil.nvim', lazy = false},
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      funcs.protectedSetup('dashboard', {
+        -- setup opts
+      })
+    end
+  }
 })
 
-vim.g.coq_settings = { auto_start = 'shut-up' } 
+-- keys setup and extensions keymap
+local _, wk = funcs.protectedCall('which-key')
+wk.register({
+  ['-'] = {'<cmd>Oil<cr>', 'open file parent'}
+})
+wk.register({
+  f = {
+    name = 'directory',
+    f = {'<cmd>Oil<cr>', 'open file parent'},
+    p = {'<cmd>Oil --float<cr>', 'open file parent in floating window'}
+  },
+  d = {
+    name = 'diagnostics',
+    f = {'<cmd>lua vim.diagnostic.goto_prev()<CR>'},
+    j = {'<cmd>lua vim.diagnostic.goto_next()<CR>'},
+    F = {
+      '<cmd>lua vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR})<CR>'},
+    J = {
+      '<cmd>lua vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR})<CR>'}
+  },
+  a = {
+    name = 'tabs',
+    t = {'<cmd>$tabnew<cr>', 'open new tab'},
+    w = {'<cmd>tabclose<cr>', 'close tab'},
+    W = {'<cmd>tabonly<cr>', 'close other tabs'},
+    ['<tab>'] = {'<cmd>tabn<cr>', 'next tab'},
+    ['<s-tab>'] = {'<cmd>tabp<cr>', 'previous tab'},
+    [','] = {'<cmd>-tabmove<cr>', 'move tab left'},
+    ['.'] = {'<cmd>+tabmove<cr>', 'move tab right'}
+  },
+  l = {
+    name = 'language server actions',
+    r = {'<cmd>lua vim.lsp.buf.rename()<CR>', 'Rename'},
+    a = {'<cmd>lua vim.lsp.buf.code_action()<CR>', 'Code Action'},
+    d = {'<cmd>lua vim.diagnostic.open_float()<CR>', 'Line Diagnostics'},
+    i = {'<cmd>LspInfo<CR>', 'LSP info'}
+  },
+  g = {
+    name = 'goto actions',
+    d = {'<cmd>lua vim.lsp.buf.definition()<CR>', 'Definition'},
+    D = {'<cmd>lua vim.lsp.buf.declaration()<CR>', 'Declaration'},
+    s = {'<cmd>lua vim.lsp.buf.signature_help()<CR>', 'Signature Help'},
+    I = {'<cmd>lua vim.lsp.buf.implementation()<CR>', 'Implementation'},
+    t = {'<cmd>lua vim.lsp.buf.type_definition()<CR>', 'Type Definition'}
+  },
+  w = {
+    name = 'whichkey',
+    a = {'<cmd>WhichKey<CR>', 'show all normal'},
+    A = {'<cmd>WhichKey \'\' v<CR>', 'show all visual'},
+    r = {'<cmd>WhichKey "<CR>', 'show all registers'},
+    m = {'<cmd>WhichKey `<CR>', 'show all marks'}
+  }
+}, {prefix = '<leader>'})
 
--- language server startup
-local ok, mason = funcs.protectedSetup('mason')
-local ok, masonlsp = funcs.protectedSetup('mason-lspconfig')
+-- completion setup
+
+vim.g.coq_settings = { auto_start = 'shut-up' }
+local _, coq = funcs.protectedCall('coq')
+
+
+-- language server and gps
+funcs.protectedSetup('mason')
+local _, masonlsp = funcs.protectedSetup('mason-lspconfig')
+local _, navic = funcs.protectedCall('nvim-navic')
+masonlsp.setup_handlers({
+  function(server_name)
+    local on_attach = function(client, bufnr)
+    end
+    local _, lspconfig = funcs.protectedCall('lspconfig')
+    lspconfig[server_name].setup(coq.lsp_ensure_capabilities({
+      on_attach = on_attach,
+      flags = {
+        debounce_text_changes = 150
+      }
+    }))
+  end
+})
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- navigation
+funcs.protectedSetup('oil')
+
 
 -- apperance
 
-local ok, fel = funcs.protectedCall('ui.feline')
-local ok, tab = funcs.protectedCall('ui.tabby')
-local ok, ibl = funcs.protectedSetup('ibl')
-
--- appearance
-
-
-
--- keymaps
-
-
-
---[[-- telescope
-local ok, builtin = funcs.protectedCall('telescope.builtin')
-if ok then
-  funcs.nmap('<leader>ff', builtin.find_files, {})
-  funcs.nmap('<leader>fg', builtin.live_grep, {})
-  funcs.nmap('<leader>fb', builtin.buffers, {})
-  funcs.nmap('<leader>fh', builtin.help_tags, {})
-end
-
--- markdown
-funcs.nmap('<leader>md', '<Plug>MarkdownPreviewToggle')
-
--- colorscheme
-
-vim.cmd([[
-  syntax enable
-  colorscheme duskfox
-  set tgc
-]]--)
-
--- language servers
---[[
-funcs.protectedSetup('mason', {})
-funcs.protectedSetup('mason-lspconfig', {})
-
--- navigation
-
-local ok, telescope = funcs.protectedSetup('telescope', {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  }
-})
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-funcs.protectedFuncCall('telescope.load_extension', 'fzf')
-
-funcs.protectedSetup('gitsigns', {})
-
--- treesitter
-
-funcs.protectedSetup('nvim-treesitter.configs', {
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  }
-})
-
--- appearance
-
 funcs.protectedCall('ui.feline')
 funcs.protectedCall('ui.tabby')
---]]
+funcs.protectedSetup('ibl', {
+  exclude = { filetypes = {
+    'lspinfo',
+    'packer',
+    'checkhealth',
+    'help',
+    '',
+    'dashboard',
+    'mason'
+  }}
+})
